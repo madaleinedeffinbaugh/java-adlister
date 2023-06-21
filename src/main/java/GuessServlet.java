262145526d92f2f22995c9302302d464328b965f
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-06-21 01:37:31 -0600

    added bonus

--- src/main/java/GuessServlet.java
+++ src/main/java/GuessServlet.java
@@ -14,14 +14,22 @@ public class GuessServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int guess = Integer.parseInt(request.getParameter("userGuess"));
-        Random rand = new Random();
+        String userMessage = "";
+//        Random rand = new Random();
 //        int secretNum = rand.nextInt(3) + 1;
         int secretNum = 2;
-        if (guess == secretNum) {
-            response.sendRedirect( "/correct?message=correct");
+        if(guess > 0 && guess < 4) {
+            if (guess == secretNum) {
+                userMessage = "You Won!";
+            } else {
+                userMessage = "You Lose!";
+            }
+            response.sendRedirect( "/correct?message=" + userMessage);
         } else {
-            response.sendRedirect("/incorrect?message=incorrect");
+            response.sendRedirect( "/guess.jsp");
         }
+
+
 
 
     }
