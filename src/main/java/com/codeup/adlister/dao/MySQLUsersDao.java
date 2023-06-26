a77d0ea0d8bea3d5df18858544530972274fb5f8
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-06-26 15:47:09 -0600

    update

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -2,7 +2,7 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
-
+import com.codeup.adlister.Configs.Config;
 import java.sql.*;
 
 public class MySQLUsersDao implements Users {
